8f503bdd9adb888619a5c244f9bc729a113870ef
omkar shivam <omkarshivam99@gmail.com>
2023-02-10 15:01:14 +0100

    initail setup

---
+++ cardscript.js
@@ -0,0 +1,203 @@
+var im=document.getElementById('im');
+var ca=document.getElementById('ca');
+var thor=document.getElementById('thor');
+var bp=document.getElementById('bp');
+var hulk=document.getElementById('hulk');
+var he=document.getElementById('he');
+var sm=document.getElementById('sm');
+var sw=document.getElementById('sw');
+
+
+var e1=document.getElementById('e1');
+var e2=document.getElementById('e2');
+var e3=document.getElementById('e3');
+var e4=document.getElementById('e4');
+var e5=document.getElementById('e5');
+var e6=document.getElementById('e6');
+var e7=document.getElementById('e7');
+var e8=document.getElementById('e8');
+
+var irong=document.getElementById('irong');
+var cag=document.getElementById('cag');
+var thg=document.getElementById('thg');
+var hulkg=document.getElementById('hulkg');
+var bpg=document.getElementById('bpg');
+var swg=document.getElementById('swg');
+var heg=document.getElementById('heg');
+var smg=document.getElementById('smg');
+
+// var mainc=document.getElementsByClassName('main-card');
+
+
+
+
+e1.onmouseover=function(){
+    
+    irong.style.transform='scale(1.5)';
+    cag.style.transform='scale(0)';
+    thg.style.transform='scale(0)';
+    hulkg.style.transform='scale(0)';
+    bpg.style.transform='scale(0)';
+    swg.style.transform='scale(0)';
+    heg.style.transform='scale(0)';
+    smg.style.transform='scale(0)';
+
+
+    im.style.left='0%';
+    ca.style.left='-100%';
+    thor.style.left='-100%';
+    bp.style.left='-100%';
+    hulk.style.left='-100%';
+    sw.style.left='-100%';
+    sm.style.left='-100%';
+    he.style.left='-100%';
+
+}
+e2.onmouseover=function(){
+    // cag.style.opacity='1';
+
+    irong.style.transform='scale(0)';
+    cag.style.transform='scale(1.15)';
+    thg.style.transform='scale(0)';
+    hulkg.style.transform='scale(0)';
+    bpg.style.transform='scale(0)';
+    swg.style.transform='scale(0)';
+    heg.style.transform='scale(0)';
+    smg.style.transform='scale(0)';
+
+    im.style.left='-100%';
+    ca.style.left='0%';
+    thor.style.left='-100%';
+    bp.style.left='-100%';
+    hulk.style.left='-100%';
+    sw.style.left='-100%';
+    sm.style.left='-100%';
+    he.style.left='-100%';
+
+}
+e3.onmouseover=function(){
+    irong.style.transform='scale(0)';
+    cag.style.transform='scale(0)';
+    thg.style.transform='scale(1.2)';
+    hulkg.style.transform='scale(0)';
+    bpg.style.transform='scale(0)';
+    swg.style.transform='scale(0)';
+    heg.style.transform='scale(0)';
+    smg.style.transform='scale(0)';
+
+
+    im.style.left='-100%';
+    ca.style.left='-100%';
+    thor.style.left='0%';
+    bp.style.left='-100%';
+    hulk.style.left='-100%';
+    sw.style.left='-100%';
+    sm.style.left='-100%';
+    he.style.left='-100%';
+
+}
+e4.onmouseover=function(){
+    irong.style.transform='scale(0)';
+    cag.style.transform='scale(0)';
+    thg.style.transform='scale(0)';
+    hulkg.style.transform='scale(1.19)';
+    bpg.style.transform='scale(0)';
+    swg.style.transform='scale(0)';
+    heg.style.transform='scale(0)';
+    smg.style.transform='scale(0)';
+
+
+    im.style.left='-100%';
+    ca.style.left='-100%';
+    thor.style.left='-100%';
+    bp.style.left='-100%';
+    hulk.style.left='0%';
+    sw.style.left='-100%';
+    sm.style.left='-100%';
+    he.style.left='-100%';
+
+}
+e5.onmouseover=function(){
+    irong.style.transform='scale(0)';
+    cag.style.transform='scale(0)';
+    thg.style.transform='scale(0)';
+    hulkg.style.transform='scale(0)';
+    bpg.style.transform='scale(1.2)';
+    swg.style.transform='scale(0)';
+    heg.style.transform='scale(0)';
+    smg.style.transform='scale(0)';
+
+
+    im.style.left='-100%';
+    ca.style.left='-100%';
+    thor.style.left='-100%';
+    bp.style.left='0%';
+    hulk.style.left='-100%';
+    sw.style.left='-100%';
+    sm.style.left='-100%';
+    he.style.left='-100%';
+
+}
+e6.onmouseover=function(){
+    irong.style.transform='scale(0)';
+    cag.style.transform='scale(0)';
+    thg.style.transform='scale(0)';
+    hulkg.style.transform='scale(0)';
+    bpg.style.transform='scale(0)';
+    swg.style.transform='scale(1.2)';
+    heg.style.transform='scale(0)';
+    smg.style.transform='scale(0)';
+
+
+    im.style.left='-100%';
+    ca.style.left='-100%';
+    thor.style.left='-100%';
+    bp.style.left='-100%';
+    hulk.style.left='-100%';
+    sw.style.left='0%';
+    sm.style.left='-100%';
+    he.style.left='-100%';
+
+}
+e7.onmouseover=function(){
+    irong.style.transform='scale(0)';
+    cag.style.transform='scale(0)';
+    thg.style.transform='scale(0)';
+    hulkg.style.transform='scale(0)';
+    bpg.style.transform='scale(0)';
+    swg.style.transform='scale(0)';
+    heg.style.transform='scale(1.5)';
+    smg.style.transform='scale(0)';
+
+
+    im.style.left='-100%';
+    ca.style.left='-100%';
+    thor.style.left='-100%';
+    bp.style.left='-100%';
+    hulk.style.left='-100%';
+    sw.style.left='-100%';
+    sm.style.left='-100%';
+    he.style.left='0%';
+
+}
+e8.onmouseover=function(){
+    irong.style.transform='scale(0)';
+    cag.style.transform='scale(0)';
+    thg.style.transform='scale(0)';
+    hulkg.style.transform='scale(0)';
+    bpg.style.transform='scale(0)';
+    swg.style.transform='scale(0)';
+    heg.style.transform='scale(0)';
+    smg.style.transform='scale(1.3)';
+
+    im.style.left='-100%';
+    ca.style.left='-100%';
+    thor.style.left='-100%';
+    bp.style.left='-100%';
+    hulk.style.left='-100%';
+    sw.style.left='-100%';
+    sm.style.left='0%';
+    he.style.left='-100%';
+
+}
+
